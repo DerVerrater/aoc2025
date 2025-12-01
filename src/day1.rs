35e64a1df12cffda69f8991b60648b2e5451df3c
Robert Garrett <robertgarrett404@gmail.com>
2025-12-01 16:24:21 -0600

    Solved part 2
    Now to clean up my code so it doesn't look like a *total* mess.

--- src/day1.rs
+++ src/day1.rs
@@ -9,6 +9,12 @@ pub fn part1() -> Result<u16, Error> {
     Ok(result)
 }
 
+pub fn part2() -> Result<u16, Error> {
+    let document = fs::read_to_string("./day1.txt").map_err(|_| Error::NoInputFile)?;
+    let result = part1_impl2(document.as_str())?;
+    Ok(result)
+}
+
 /// This is the actual implementation for part 1
 ///
 /// It is private so that main.rs doesn't need to know this thing secretly
@@ -20,6 +26,21 @@ fn part1_impl(puzzle_sequence: &str) -> Result<u16, Error> {
             break;
         };
         dial.rotate(parse_operation(line)?);
+    }
+    Ok(dial.times_at_zero)
+}
+
+/// Part 2 implementation
+///
+/// Because I'm too lazy to set up a dependency-injection thing to select
+/// between the two "rotate" methods.
+fn part1_impl2(puzzle_sequence: &str) -> Result<u16, Error> {
+    let mut dial = Dial::new();
+    for line in puzzle_sequence.split('\n') {
+        if line.is_empty() {
+            break;
+        };
+        dial.rotate2(parse_operation(line)?);
     }
     Ok(dial.times_at_zero)
 }
@@ -47,6 +68,58 @@ impl Dial {
             self.times_at_zero += 1;
         }
     }
+
+    fn rotate2(&mut self, amount: i16) {
+        println!("Dial [{}], turn {}", self.current, amount);
+        // Compute the next position
+        let pos = ((self.current + amount) % 100) as i8;
+        let next = {
+            // Modulo position back into (-100, 100) range
+            // If negative, take it's compliment as final value.
+            if pos.is_negative() {
+                // now it's positive, between 0 and 100. Convert to u8
+                (100 + pos) as u8
+            } else {
+                // it's already positive, between 0 and 100. Convert to u8
+                pos as u8
+            }
+        };
+        println!(" ->> Next [{}], ({})", next, pos);
+        
+        // Count one towards times_at_zero if
+        // 1. next pos *is* 0
+        // 2. going up but next < current (happens after overflow & wrap)
+        // 3. going down but next > current (happens after underflow & wrap)
+        if (next == 0) {
+            self.times_at_zero += 1;
+            println!(" ->> Landed on zero, +1");
+        // Guard against starting from zero. If we're already there, wrapping
+        // doesn't count because we didn't touch it *again*. We were already
+        // touching it, so counting the wrap would be double counting the touch.
+        } else if self.current != 0 {
+            if (amount.is_negative() && ((self.current as u8) < next)) {
+                self.times_at_zero += 1;
+                println!(" ->> Rolled under, +1");
+            } else if (amount.is_positive() && ((self.current as u8) > next)) {
+                self.times_at_zero += 1;
+                println!(" ->> Rolled over, +1");
+            } else {
+                println!(" --> No zero-cross. Continue");
+            }
+        } else {
+            println!(" --> Already at zero, skipping this count.");
+        }
+
+        // Count how many whole wraps might have happened (e.g.: `amount=250`)
+        let loops = (amount / 100).unsigned_abs();
+        if loops > 0 {
+            println!(" ->> Whole wraps: +{}", loops);
+        }
+        self.times_at_zero += loops as u16;
+
+        // Finally, update current pos
+        self.current = next as i16;
+    }
 }
 
 fn parse_operation(text: &str) -> Result<i16, Error> {
@@ -69,6 +142,13 @@ mod test {
     fn p1() {
         let expected = 3;
         let computed = part1_impl(INSTRUCTIONS).unwrap();
+        assert_eq!(computed, expected);
+    }
+
+    #[test]
+    fn p2() {
+        let expected = 6;
+        let computed = part1_impl2(INSTRUCTIONS).unwrap();
         assert_eq!(computed, expected);
     }
 
--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,12 @@ fn main() {
                     Err(err) => err.to_string(),
                 };
                 println!("Day 1, Part 1: Result {}", p1);
+
+                let p2 = match day1::part2() {
+                    Ok(num) => num.to_string(),
+                    Err(err) => err.to_string(),
+                };
+                println!("Day 1, Part 2: Result {}", p2);
             }
             _ => {
                 eprintln!("Day is not done yet, or is an unrecognized value.");
