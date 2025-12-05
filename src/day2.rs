985a2904607488762a40943da1945c1cea040833
Robert Garrett <robertgarrett404@gmail.com>
2025-12-05 10:37:04 -0700

    Implement Day 2 part 1

---
+++ src/day2.rs
@@ -0,0 +1,52 @@
+use crate::{Error, Result};
+use pcre2::bytes::Regex;
+use std::fs;
+
+pub fn part1() -> Result<i64> {
+    let document = fs::read_to_string("./day2.txt").map_err(|_| Error::NoInputFile)?;
+    let result = part1_impl(document.as_str())?;
+    Ok(result)
+}
+
+fn part1_impl(input: &str) -> Result<i64> {
+    let ranges: Vec<(i64, i64)> = input
+        .split(",")
+        .map(|range| -> Result<(_, _)> {
+            let mut i = range.split("-");
+            Ok((
+                i.next()
+                    .and_then(|text| text.parse::<i64>().ok())
+                    .ok_or(Error::Parsing)?,
+                i.next()
+                    .and_then(|text| text.parse::<i64>().ok())
+                    .and_then(|num| Some(num + 1))
+                    .ok_or(Error::Parsing)?,
+            ))
+        })
+        .collect::<Result<_>>()?;
+    
+    let reg = Regex::new(r"^(\d+)\1$").unwrap();
+    let mut sum: i64 = 0;
+    for (start, stop) in ranges {
+        // scan range looking for repeated patterns
+        for idx in start as i64..stop as i64 {
+            let stringified = idx.to_string();
+            if reg.is_match(stringified.as_bytes()).unwrap() {
+                sum += idx;
+            }
+        }
+    }
+    Ok(sum)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn p1() {
+        assert_eq!(1227775554, part1_impl(PRODUCT_RANGES).unwrap());
+    }
+
+    const PRODUCT_RANGES: &str = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{env, fmt::Display};
 
 mod day1;
+mod day2;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -18,6 +19,13 @@ fn main() {
                     Err(err) => err.to_string(),
                 };
                 println!("Day 1, Part 2: Result {}", p2);
+            }
+            "day2" | "2" | "d2" => {
+                let p1 = match day2::part1() {
+                    Ok(num) => num.to_string(),
+                    Err(err) => err.to_string(),
+                };
+                println!("Day 2, Part 1: Result {}", p1);
             }
             _ => {
                 eprintln!("Day is not done yet, or is an unrecognized value.");
